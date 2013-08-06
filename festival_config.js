6cc8f9079e6de41d2e67db6c37c80be5c34daf3b
unknown <rreuter@CIN2-54.CIN2.creditinfonet.com>
2013-08-06 13:46:02 -0400

    Updated information and lineup for 2013

--- festival_config.js
+++ festival_config.js
@@ -13,15 +13,15 @@ var _fest_timing = {
 
 // You can artifically adjust the start time to ensure that the lineup will render correctly at different times by 
 // setting a date here
-//_fest_timing.current_time = new Date(2012, 11, 21, 23, 59); // testing after the festival
-//_fest_timing.current_time = new Date(2012, 7, 10, 20, 10); // testing 
-//_fest_timing.current_time = new Date(2012, 7, 10, 21, 59, 50); // testing  schedule change
-//_fest_timing.current_time = new Date(2012, 7, 11, 6, 0, 0); // testing no one playing
+//_fest_timing.current_time = new Date(2013, 11, 21, 23, 59); // testing after the festival
+//_fest_timing.current_time = new Date(2013, 7, 10, 20, 10); // testing 
+//_fest_timing.current_time = new Date(2013, 7, 10, 21, 29, 50); // testing  schedule change
+//_fest_timing.current_time = new Date(2013, 7, 11, 6, 0, 0); // testing no one playing
 
 var _fest_messages =  {
 	no_one_currently_playing : "No acts are currently playing. Please check the schedule for when the next act is playing.",
 	pre_festival_message : "<h2 style='text-align:center'>Miami Valley Music Fest</h2><div style='text-align: center;'>" + 
-		"<p style='font-weight: bold; font-size: larger;'>August 11th - 12th</p>" +
+		"<p style='font-weight: bold; font-size: larger;'>August 9th - 10th</p>" +
 		"<p><img src='images/mvmf.jpg' alt='Miami Valley Music Fest'/></p>" + 
 		"<p style='font-weight: bold;'><a href='http://www.miamivalleymusicfest.com/tickets/' target='_blank'>Buy Tickets Here</a></p>" +
 		"</div>",
@@ -46,7 +46,7 @@ var _fest_messages =  {
 };
 
 var _fest_pre_festival_message = "<h2 style='text-align:center'>Miami Valley Music Fest</h2><div style='text-align: center;'>" + 
-	"<p style='font-weight: bold; font-size: larger;'>August 11th - 12th</p>" +
+	"<p style='font-weight: bold; font-size: larger;'>August 9th - 10th</p>" +
 	"<p><img src='images/mvmf.jpg' alt='Miami Valley Music Fest'/></p>" + 
 	"<p style='font-weight: bold;'><a href='http://www.miamivalleymusicfest.com/tickets/' target='_blank'>Buy Tickets Here</a></p>" +
 	"</div>";
@@ -59,14 +59,14 @@ var _fest_post_festival_message = "<div style='text-align: center;'>" +
 // Page Configuration Options
 var _fest_info = {
 	page_title : "Miami Valley Music Fest",
-	copyright : "&copy; 2012 <a href='http://www.miamivalleymusicfest.com/'>Miami Valley Music Fest</a>"
+	copyright : "&copy; 2013 <a href='http://www.miamivalleymusicfest.com/'>Miami Valley Music Fest</a>"
 };
 
 // Sharing Information
 var _fest_sharing_info = {
 	name : "Miami Valley Music Fest Lineup",
-	url :"www.ninefalsesuns.com/lineup",
-	description : "Mobile site with the lineup for the 2012 Miami Valley Music Fest."
+	url :"m.miamivalleymusicfest.com",
+	description : "Mobile site with the lineup for the 2013 Miami Valley Music Fest."
 };
 
 // Festival Day Definitions
@@ -77,14 +77,14 @@ var _fest_days =
 [
 	{
 		name : "Friday",
-		start_date : new Date(2012, 7, 10, 11, 0),
-		end_date : new Date(2012, 7, 11, 4, 59),
+		start_date : new Date(2013, 7, 9, 11, 0),
+		end_date : new Date(2013, 7, 10, 4, 59),
 		id : "friday"
 	},
 	{
 		name : "Saturday",
-		start_date: new Date(2012, 7, 11, 5, 0),
-		end_date : new Date(2012, 7, 12, 11, 0),
+		start_date: new Date(2013, 7, 10, 5, 0),
+		end_date : new Date(2013, 7, 11, 11, 0),
 		id : "saturday"
 	}
 ];
@@ -95,176 +95,163 @@ var _fest_days =
 function get_festival_data()
 {
 	// *** Charity Stage
-	var charity_stage = new Stage('Charity Rocks! Stage One');
+	var stage_1 = new Stage('Stage 1 - Main Stage');
 	
 	// Friday
-	var ldap = new Artist('The Lee Dynes Acoustic Project', '', '');
-	charity_stage.add_performance(ldap, new Date(2012, 7, 10, 16, 00), 60);
+	var sol = new Artist('SOL', '', '');
+	stage_1.add_performance(sol, new Date(2013, 7, 9, 16, 00), 60);
 	
-	var dcc = new Artist('DC Connection', '', '');
-	charity_stage.add_performance(dcc, new Date(2012, 7, 10, 18, 00), 60);
+	var aliver_hall = new Artist('Aliver Hall', '', '');
+	stage_1.add_performance(aliver_hall, new Date(2013, 7, 9, 19, 00), 60);
 	
-	var cmb = new Artist('Clark Manson Band', 'http://clarkmansonband.com/', '');
-	charity_stage.add_performance(cmb, new Date(2012, 7, 10, 20, 00), 60);
-	
-	var signsol = new Artist('Signs of Life - The Essence of Pink Floyd', 'http://www.signsoffloyd.com/fr_welcome.cfm', '');
-	charity_stage.add_performance(signsol, new Date(2012, 7, 10, 22, 00), 60);
+	var spikedrivers = new Artist('Spikedrivers', '', '');
+	stage_1.add_performance(spikedrivers, new Date(2013, 7, 9, 21, 30), 90);
 	
 	// Saturday
-	var sl = new Artist('Scott Lee', '', '');
-	charity_stage.add_performance(sl, new Date(2012, 7, 11, 10, 30), 60);
+	var such_a_night = new Artist('Such a Night (The Last Waltz)', '', '');
+	stage_1.add_performance(such_a_night, new Date(2013, 7, 10, 11, 00), 120);
 	
-	var ahb = new Artist('The Al Holbrook Band', 'http://www.facebook.com/pages/The-Al-Holbrook-Band/268856187844', '');
-	charity_stage.add_performance(ahb, new Date(2012, 7, 11, 12, 30), 60);
+	var subterranean_house_band = new Artist('Subterranean House Band', '', '');
+	stage_1.add_performance(subterranean_house_band, new Date(2013, 7, 10, 14, 00), 60);
 	
-	var gf = new Artist('Groovestone Fusion Band', 'http://www.reverbnation.com/groovestonefusion', '');
-	charity_stage.add_performance(gf, new Date(2012, 7, 11, 14, 30), 60);
+	var grover = new Artist('Grover', '', '');
+	stage_1.add_performance(grover, new Date(2013, 7, 10, 16, 00), 60);
 	
-	var tdc = new Artist('The Demolition Crew', 'http://www.facebook.com/demolition.crew.7', '');
-	charity_stage.add_performance(tdc, new Date(2012, 7, 11, 16, 30), 60);
+	var glostik_willy = new Artist('Glostik Willy', '', '');
+	stage_1.add_performance(glostik_willy, new Date(2013, 7, 10, 18, 00), 60);
 	
-	var mperk = new Artist('Mike Perkins', 'http://www.mikeperkins.net/', '');
-	charity_stage.add_performance(mperk, new Date(2012, 7, 11, 18, 30), 60);
+	var demolition_crew = new Artist('Demolition Crew', '', '');
+	stage_1.add_performance(demolition_crew, new Date(2013, 7, 10, 20, 00), 60);
 	
-	var spikedrivers = new Artist('The Spikedrivers', 'http://www.thespikedrivers.com/', '');
-	charity_stage.add_performance(spikedrivers, new Date(2012, 7, 11, 21, 00), 75);
-	
-	var hookah = new Artist('Ekoostik Hookah', 'http://www.ekoostik.com/', '');
-	charity_stage.add_performance(hookah, new Date(2012, 7, 12, 0, 00), 75);
+	var signs_of_life = new Artist('Signs of Life', '', '');
+	stage_1.add_performance(signs_of_life, new Date(2013, 7, 10, 23, 00), 75);
+
 	
 	// *** WYSO Stage
-	var wyso_stage = new Stage("WYSO Stage");
+	var stage_2 = new Stage("Stage 2");
 	
 	// Friday
-	var nw = new Artist('Noah Wotherspoon', 'http://www.noahwotherspoon.com/', '');
-	wyso_stage.add_performance(nw, new Date(2012, 7, 10, 17, 00), 60);
+	var terrapin_moon = new Artist('Terrapin Moon', 'http://terrapinmoon.net/', '');
+	stage_2.add_performance(terrapin_moon, new Date(2013, 7, 9, 18, 00), 60);
 	
-	var tp = new Artist('Terrapin Moon', 'http://terrapinmoon.net/', '');
-	wyso_stage.add_performance(tp, new Date(2012, 7, 10, 19, 00), 60);
+	var mike_perkins = new Artist('Mike Perkins', '', '');
+	stage_2.add_performance(mike_perkins, new Date(2013, 7, 9, 20, 00), 60);
 	
-	var hig_made = new Artist('Higgins-Madewell', 'http://www.higginsmadewell.com/', '');
-	wyso_stage.add_performance(hig_made, new Date(2012, 7, 10, 21, 00), 60);
-	
-	var gw =  new Artist('Glostik Willy', 'http://glostikwilly.com/fr_gigs.cfm', '');
-	wyso_stage.add_performance(gw, new Date(2012, 7, 11,0, 00), 60);
+	var lost_on_iddings = new Artist('Lost on Iddings/Bellydancers', 'http://www.lostoniddings.com/', '');
+	stage_2.add_performance(lost_on_iddings, new Date(2013, 7, 9, 23, 30), 75);
 	
 	// Saturday
-	var accapella =  new Artist('Accapella Group', '', '');
-	wyso_stage.add_performance(accapella, new Date(2012, 7, 11, 9, 45), 60);
+	var will_c_family =  new Artist('Will C and F.A.M.!.L.Y.', '', '');
+	stage_2.add_performance(will_c_family, new Date(2013, 7, 10, 13, 00), 60);
 	
-	var swrb = new Artist('Stillwater River Band', 'https://www.facebook.com/thestillwaterriverband', '');
-	wyso_stage.add_performance(swrb, new Date(2012, 7, 11, 11, 30), 60);
+	var jdreads_mystiks = new Artist('Jonny Dreads and The Mystiks', '', '');
+	stage_2.add_performance(jdreads_mystiks, new Date(2013, 7, 10, 15, 00), 60);
 	
-	var soulrebels = new Artist('Soul Rebels', 'http://www.reverbnation.com/soulrebelsmusic', '');
-	wyso_stage.add_performance(soulrebels, new Date(2012, 7, 11, 13, 30), 60);
+	var jones_for_revival = new Artist('Jones For Revival', '', '');
+	stage_2.add_performance(jones_for_revival, new Date(2013, 7, 10, 17, 00), 60);
 	
-	var grover = new Artist('Grover', 'http://www.facebook.com/groverfunk', '');
-	wyso_stage.add_performance(grover, new Date(2012, 7, 11, 15, 30), 60);
+	var jah_soul = new Artist('Jah Soul', 'http://www.jahsoulfamily.com', '');
+	stage_2.add_performance(jah_soul, new Date(2013, 7, 10, 19, 00), 60);
 	
-	var blue_moon_soup = new Artist('Blue Moon Soup', 'http://www.reverbnation.com/bluemoonsoup', '');
-	wyso_stage.add_performance(blue_moon_soup, new Date(2012, 7, 11, 18, 30), 60);
-	
-	var sol =  new Artist('SOL', 'http://www.facebook.com/pages/SOL/116461645078819', '');
-	wyso_stage.add_performance(sol, new Date(2012, 7, 11, 20, 00), 60);
-	
-	var aliver_hall = new Artist('Aliver Hall', 'http://www.aliverhall.com/', '');
-	wyso_stage.add_performance(aliver_hall, new Date(2012, 7, 11, 23, 00), 60);
+	var big_damn_jam = new Artist('Big Damn Jam', '', '');
+	stage_2.add_performance(big_damn_jam, new Date(2013, 7, 10, 21, 00), 60);
 	
 	// *** Indoor Stage
-	var indoor_stage = new Stage('Indoor Stage');
+	var stage_3 = new Stage('Stage 3');
 	
 	// Friday
-	var mc = new Artist('Mark Cantwil', 'http://www.reverbnation.com/markcantwil', '');
-	indoor_stage.add_performance(mc, new Date(2012, 7, 10, 17, 00), 60);
+	var fables = new Artist('Fables', '', '');
+	stage_3.add_performance(fables, new Date(2013, 7, 9, 17, 30), 60);
 	
-	var btlg = new Artist('Bootleg', 'http://www.myspace.com/bootlegbluzband', '');
-	indoor_stage.add_performance(btlg, new Date(2012, 7, 10, 18, 30), 60);
+	var fox_n_hounds = new Artist('Fox-n-Hounds', '', '');
+	stage_3.add_performance(fox_n_hounds, new Date(2013, 7, 9, 19, 00), 60);
 	
-	var elbk = new Artist('Electrobek', '', '');
-	indoor_stage.add_performance(elbk, new Date(2012, 7, 10, 20, 00), 60);
+	var city_of_kings = new Artist('City of Kings', '', '');
+	stage_3.add_performance(city_of_kings, new Date(2013, 7, 9, 20, 30), 60);
 	
-	var nfs = new Artist('Nine False Suns', 'http://www.ninefalsesuns.com', 'Some band');
-	indoor_stage.add_performance(nfs, new Date(2012, 7, 10, 21, 30), 60);
+	var paradijm_shift = new Artist('Paradijm Shift', '', '');
+	stage_3.add_performance(paradijm_shift, new Date(2013, 7, 9, 22, 00), 60);
 	
-	var sb = new Artist('Shank Bones', 'http://www.facebook.com/ShankBones', '');
-	indoor_stage.add_performance(sb, new Date(2012, 7, 10, 23, 00), 60);
-	
-	var hp = new Artist('Haunted Palace', 'http://www.reverbnation.com/hauntedpalace', '');
-	indoor_stage.add_performance(hp, new Date(2012, 7, 11,0, 30), 90);
-	
-	var bmm = new Artist('Boogie Matrix Mechanism', 'http://boogiematrix.com/', 'Dancing band');
-	indoor_stage.add_performance(bmm, new Date(2012, 7, 11,02, 00), 90);
-	
+	var dc_connection = new Artist('DC Connection', '', '');
+	stage_3.add_performance(dc_connection, new Date(2013, 7, 9, 23, 30), 60);
+
 	// Saturday
-	var giggle_grass = new Artist('Giggle Grass', '', '');
-	indoor_stage.add_performance(giggle_grass, new Date(2012, 7, 11, 11, 30), 60);
+	var mystical_flutes = new Artist('Mystical Flutes and Drums', '', '');
+	stage_3.add_performance(mystical_flutes, new Date(2013, 7, 10, 10, 00), 60);
 	
-	var paradigm_shift = new Artist('Paradigm Shift', 'http://www.facebook.com/pages/Paradijm-Shift/134930403184170', '');
-	indoor_stage.add_performance(paradigm_shift, new Date(2012, 7, 11, 14, 30), 60);
+	var TBA = new Artist('TBA', '', '');
+	stage_3.add_performance(TBA, new Date(2013, 7, 10, 12, 00), 60);
+	stage_3.add_performance(TBA, new Date(2013, 7, 10, 13, 30), 60);
 	
-	var city_kings = new Artist('City of Kings', 'http://cityofkings.org/the-band/', '');
-	indoor_stage.add_performance(city_kings, new Date(2012, 7, 11, 16, 00), 60);
+	var emily_lost_cat = new Artist('Emily and The Lost Cat Ramblers', '', '');
+	stage_3.add_performance(emily_lost_cat, new Date(2013, 7, 10, 15, 00), 60);
 	
-	var slight_reb = new Artist('Slight Rebellion', 'http://www.reverbnation.com/theslightrebellion', '');
-	indoor_stage.add_performance(slight_reb, new Date(2012, 7, 11, 17, 30), 60);
+	var belly_dancers = new Artist('Belly Dancers', '', '');
+	stage_3.add_performance(belly_dancers, new Date(2013, 7, 10, 16, 30), 60);
 	
-	var m87 = new Artist('M87', 'http://www.reverbnation.com/m87music', '');
-	indoor_stage.add_performance(m87, new Date(2012, 7, 11, 19, 00), 60);
+	var prophets_mire = new Artist('Prophets Mire', '', '');
+	stage_3.add_performance(prophets_mire, new Date(2013, 7, 10, 18, 00), 60);
 	
-	var tat_roots = new Artist('Tattered Roots', 'http://www.reverbnation.com/tatteredroots', '');
-	indoor_stage.add_performance(tat_roots, new Date(2012, 7, 11, 20, 30), 60);
+	var kris_hanson = new Artist('Kris Hanson', '', '');
+	stage_3.add_performance(kris_hanson, new Date(2013, 7, 10, 19, 30), 60);
 	
-	var scotty_b = new Artist('Scotty Bratcher', 'http://www.scottybratcher.com/', '');
-	indoor_stage.add_performance(scotty_b, new Date(2012, 7, 11, 22, 00), 60);
+	var elektrobek = new Artist('Elektrobek', '', '');
+	stage_3.add_performance(elektrobek, new Date(2013, 7, 10, 21, 00), 60);
 	
-	var skeetones = new Artist('The Skeetones', 'http://www.skeetones.com/', '');
-	indoor_stage.add_performance(skeetones, new Date(2012, 7, 12,02, 00), 90);
+	var box_truck_rescue = new Artist('Box Truck Rescue', '', '');
+	stage_3.add_performance(box_truck_rescue, new Date(2013, 7, 10, 22, 30), 60);
+	
+	var hieronymus_project = new Artist('The Hieronymus Project', '', '');
+	stage_3.add_performance(hieronymus_project, new Date(2013, 7, 11, 00, 00), 60);
 	
 	// *** Acoustic Stage
 	// Friday
 	var acoustic_stage = new Stage("Acoustic Stage");
 	
-	var er = new Artist('Evan Ray', 'http://www.facebook.com/evanraymusic', '');
-	acoustic_stage.add_performance(er, new Date(2012, 7, 10, 17, 00), 60);
+	var joe_gillis = new Artist('Joeseph Gillis', '', '');
+	acoustic_stage.add_performance(joe_gillis, new Date(2013, 7, 9, 17, 45), 60);
 	
-	var jjw = new Artist('Jack and Joe Waters', 'http://www.myspace.com/joejackwaters', '');
-	acoustic_stage.add_performance(jjw, new Date(2012, 7, 10, 18, 00), 60);
+	var cinder_home = new Artist('Cinder Home', '', '');
+	acoustic_stage.add_performance(cinder_home, new Date(2013, 7, 9, 20, 45), 60);
 	
-	var bd = new Artist('Belly Dancing Troupe', '', '');
-	acoustic_stage.add_performance(bd, new Date(2012, 7, 10, 19, 00), 60);
+	var far_from_eden = new Artist('Far From Eden', '', '');
+	acoustic_stage.add_performance(far_from_eden, new Date(2013, 7, 9, 22,15), 60);
 	
-	var tony_h = new Artist('Tony Herdman', 'http://www.facebook.com/tony.herdman.9', '');
-	acoustic_stage.add_performance(tony_h, new Date(2012, 7, 10, 20, 00), 50);	
-	
-	var suzy = new Artist('Suzy', '', '');
-	acoustic_stage.add_performance(suzy, new Date(2012, 7, 10, 21, 00), 60);
+	var stillwater_river_band = new Artist('Stillwater River Band ', '', '');
+	acoustic_stage.add_performance(stillwater_river_band, new Date(2013, 7, 10, 00, 00), 75);	
 	
 	// Saturday
-	var open_stage = new Artist('Open Stage', '', '');
-	acoustic_stage.add_performance(open_stage, new Date(2012, 7, 11, 12, 00), 90);
-	
 	var megan_o = new Artist('Megan Osman', '', '');
-	acoustic_stage.add_performance(megan_o, new Date(2012, 7, 11, 14, 00), 50);	
+	acoustic_stage.add_performance(megan_o, new Date(2013, 7, 10, 12, 00), 75);	
 	
-	var s_d = new Artist('Schwartz/Devore', '', '');
-	acoustic_stage.add_performance(s_d, new Date(2012, 7, 11, 15, 00), 50);	
+	var sweet_betsy = new Artist('Sweet Betsy', '', '');
+	acoustic_stage.add_performance(sweet_betsy, new Date(2013, 7, 10, 13, 30), 75);	
 	
-	var bret_h = new Artist('Bret Heckerman', '', '');
-	acoustic_stage.add_performance(bret_h, new Date(2012, 7, 11, 16, 00), 50);	
+	var ambrogi_bros = new Artist('The Bros. Ambrogi', '', '');
+	acoustic_stage.add_performance(ambrogi_bros, new Date(2013, 7, 10, 15, 00), 75);	
 	
-	acoustic_stage.add_performance(bd, new Date(2012, 7, 11, 17, 00), 60);
+	var scott_lee = new Artist('Scott Lee and the Whiskey River Boys', '', '');
+	acoustic_stage.add_performance(scott_lee, new Date(2013, 7, 10, 16, 30), 75);
 	
-	var daniel_d = new Artist('Daniel Dye', 'http://danieldyemusic.com/', '');
-	acoustic_stage.add_performance(daniel_d, new Date(2012, 7, 11, 18, 00), 50);	
+	var sharon_lane = new Artist('Sharon Lane', '', '');
+	acoustic_stage.add_performance(sharon_lane, new Date(2013, 7, 10, 18, 00), 75);	
 	
-	var kris_h = new Artist('Kris Hanson', 'http://www.reverbnation.com/krishansonsoloact', '');
-	acoustic_stage.add_performance(kris_h, new Date(2012, 7, 11, 19, 00), 50);	
+	var higgins_madewell = new Artist('Higgins-Madewell', '', '');
+	acoustic_stage.add_performance(higgins_madewell, new Date(2013, 7, 10, 17, 30), 75);	
 
+	var the_repeating_arms = new Artist('The Repeating Arms', '', '');
+	acoustic_stage.add_performance(the_repeating_arms, new Date(2013, 7, 10, 21, 30), 75);	
+	
+	var gin_soaked_angels = new Artist('Gin Soaked Angels', '', '');
+	acoustic_stage.add_performance(gin_soaked_angels, new Date(2013, 7, 10, 32, 00), 75);	
+	
+	var rumpke_mountain_boys = new Artist('Rumpe Mountain Boys', '', '');
+	acoustic_stage.add_performance(rumpke_mountain_boys, new Date(2013, 7, 11, 00, 30), 90);	
 
 	var festival = new Festival("Miami Valley Music Fest");
-	festival.add_stage(charity_stage);
-	festival.add_stage(wyso_stage);
-	festival.add_stage(indoor_stage);
+	festival.add_stage(stage_1);
+	festival.add_stage(stage_2);
+	festival.add_stage(stage_3);
 	festival.add_stage(acoustic_stage);
 	
 	return festival;
